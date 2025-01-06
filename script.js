cadaa0eb7930e2ab890e5ab6dfb71362a0b9fd9a
Maxence <74203792+Miscuit62@users.noreply.github.com>
2025-01-06 21:38:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let compteur = 0;
+
+function Deplacer() {
+    const clavier = document.getElementById('clavier');
+    const affichage = document.querySelector('.affichage');
+    const nouveauDiv = document.createElement('div');
+
+    nouveauDiv.innerHTML = clavier.value;
+    nouveauDiv.classList.add('texte');
+    if (compteur % 2 === 0) {
+        nouveauDiv.classList.add('droite');
+    } else {
+        nouveauDiv.classList.add('gauche');
+    }
+
+    affichage.appendChild(nouveauDiv);
+    clavier.value = '';
+    compteur++;
+}
+
+function ModifierAspectClavier() {
+    const clavier = document.getElementById('clavier');
+    clavier.style.resize = 'vertical';
+    clavier.style.overflow = 'auto';
+    clavier.style.width = '50%';
+    clavier.style.minWidth = '400px';
+}
+
+function Demarre() {
+    ModifierAspectClavier();
+}
+
+window.addEventListener('load', Demarre);
+
+const boutonAjouter = document.getElementById('ajouter');
+boutonAjouter.addEventListener('click', Deplacer);
